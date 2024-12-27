ba1f428dcba265b96cdbbf625f308ccd7c8972e6
Sourav_126 <souravarora285@gmail.com>
2024-12-27 23:20:04 +0530

    url

--- src/hooks/useSocket.tsx
+++ src/hooks/useSocket.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
-const WS_URL = "https://40f5-1-187-225-47.ngrok-free.app";
+const WS_URL =
+  "  https://fa55-2401-4900-5f97-fce8-4158-2670-3247-5e30.ngrok-free.app";
 
 export const useSocket = () => {
   const [socket, setSocket] = useState<WebSocket | null>();
